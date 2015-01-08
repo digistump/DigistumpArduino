d8e0256a9190faafba00f778bab60687e3278951
Erik Tylek Kettenburg <support@digistump.com>
2015-01-07 17:48:49 -0800

    Add more examples - fix robot example

--- hardware/digistump/avr/libraries/Digispark_Examples/WiFiServerRobot/WiFiServerRobot.ino
+++ hardware/digistump/avr/libraries/Digispark_Examples/WiFiServerRobot/WiFiServerRobot.ino
@@ -67,55 +67,56 @@ void loop() {
 void sendOK(){
  sendResponse(F("OK")); 
 }
+ 
 void botForward(int botSpeed){
- digitalWrite(2,HIGH); 
- digitalWrite(5,HIGH);
- analogWrite(0,botSpeed);
- analogWrite(1,botSpeed);
+ digitalWrite(2, HIGH); 
+ digitalWrite(5, HIGH);
+ analogWrite(0, 255 - botSpeed);
+ analogWrite(1, 255 - botSpeed);
 }
-
+ 
 void botReverse(int botSpeed){
- digitalWrite(2,LOW); 
- digitalWrite(5,LOW);
- analogWrite(0,botSpeed);
- analogWrite(1,botSpeed);
+ digitalWrite(2, LOW); 
+ digitalWrite(5, LOW);
+ analogWrite(0, botSpeed);
+ analogWrite(1, botSpeed);
 }
-
+ 
 void botRight(int botSpeed){
- digitalWrite(2,HIGH); 
- digitalWrite(5,LOW);
- analogWrite(0,botSpeed);
- analogWrite(1,0);
+ digitalWrite(2, LOW); 
+ digitalWrite(5, HIGH);
+ analogWrite(0, 0);
+ analogWrite(1, 255 - botSpeed);
 }
-
+ 
 void botHardRight(int botSpeed){
- digitalWrite(2,HIGH); 
- digitalWrite(5,LOW);
- analogWrite(0,botSpeed);
- analogWrite(1,botSpeed);
+ digitalWrite(2, LOW); 
+ digitalWrite(5, HIGH);
+ analogWrite(0, botSpeed);
+ analogWrite(1, 255 - botSpeed);
 }
-
+ 
 void botLeft(int botSpeed){
- digitalWrite(2,LOW); 
- digitalWrite(5,HIGH);
- analogWrite(0,0);
- analogWrite(1,botSpeed);
+ digitalWrite(2, HIGH); 
+ digitalWrite(5, LOW);
+ analogWrite(0, 255 - botSpeed);
+ analogWrite(1, 0);
 }
-
+ 
 void botHardLeft(int botSpeed){
- digitalWrite(2,LOW); 
- digitalWrite(5,HIGH);
- analogWrite(0,botSpeed);
- analogWrite(1,botSpeed);
+ digitalWrite(2, HIGH); 
+ digitalWrite(5, LOW);
+ analogWrite(0, 255 - botSpeed);
+ analogWrite(1, botSpeed);
 }
-
+ 
 void botStop(){
  digitalWrite(2,LOW); 
  digitalWrite(5,LOW);
  analogWrite(0,0);
  analogWrite(1,0);
 }
-
+ 
 void botInit(){
  pinMode(0,OUTPUT);
  pinMode(1,OUTPUT);
