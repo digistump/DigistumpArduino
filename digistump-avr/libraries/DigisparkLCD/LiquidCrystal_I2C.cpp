822a2e4f5596b22bb8ec0c64d6ed3e6156fe5337
Erik Kettenburg <support@digistump.com>
2016-02-12 19:09:37 -0800

    update spi, fix lcd error

--- digistump-avr/libraries/DigisparkLCD/LiquidCrystal_I2C.cpp
+++ digistump-avr/libraries/DigisparkLCD/LiquidCrystal_I2C.cpp
@@ -4,11 +4,8 @@
 #include <inttypes.h>
 #include "Arduino.h"
 
-#if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny2313__) || defined(__AVR_ATtiny167__)
-  #include "TinyWireM.h"      // include this if ATtiny85 or ATtiny2313
-#else
-  #include <Wire.h>           // original lib include
-#endif
+ #include "TinyWireM.h"      // include this if ATtiny85 or ATtiny2313
+
 
 // When the display powers up, it is configured as follows:
 //
